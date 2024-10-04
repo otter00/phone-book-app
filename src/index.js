b9d362fc96657d89242e9814a627d76740d8d40e
otter00 <ivlevanastia2001@mail.ru>
2024-10-04 15:33:50 +0300

    redux changes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Redux dependecies
+import { Provider } from 'react-redux'
+import store from './store'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Provider>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
